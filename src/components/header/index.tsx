6e83594e648c3421f06a01454e00c0a03e7bf8de
Tucker Bickler <tuckersworld@gmail.com>
2021-10-26 19:09:01 -0600

    set language

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,16 @@
 import { Link } from "gatsby";
 import * as React from "react";
+import { Helmet } from "react-helmet";
 
 import "./index.scss";
 
 const Header = () => (
     <header className="Header">
+        <Helmet
+            htmlAttributes={{
+                lang: "en",
+            }}
+        />
         <Link to="/">
             <h1>trey allen</h1>
         </Link>
@@ -20,7 +26,9 @@ const Header = () => (
                     </a>
                 </li>
                 <li>
-                    <a href="mailto:tmerkord98@gmail.com">tmerkord98@gmail.com</a>
+                    <a href="mailto:tmerkord98@gmail.com">
+                        tmerkord98@gmail.com
+                    </a>
                 </li>
             </ul>
         </nav>
